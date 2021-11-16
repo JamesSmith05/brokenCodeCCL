3ddd4c7a123ef97c4b62d3da0218e61debdf63ea
chris.lloyd <learntocode2015@outlook.com>
2021-11-16 11:56:26 +0000

    1st commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        System.out.println("How many numbers?");
+        int total = input.nextInt();
+
+        for (int i = 0; i < total; i++) {
+            nums.add(input.nextInt());
+        }
+
+        sort(nums);
+        print(nums);
+
+    }
+
+    public static void sort(ArrayList<Integer> numbers) {
+        boolean sorted = false;
+        while (sorted == false) {
+            sorted = true;
+            for (int i = 1; i < numbers.size(); i++) {
+                if(numbers.get(i) > numbers.get(i+1)){
+                    int temp = numbers.get(i);
+                    numbers.set(i, numbers.get(i+1)) ;
+                    numbers.set(i+1, temp) ;
+                    sorted = false;
+                }
+            }
+        }
+
+    }
+
+    public static void print(ArrayList numbers) {
+
+        for (int i = numbers.size(); i > 0; i--) {
+            System.out.println(numbers.get(i));
+        }
+    }
+
+
+}
